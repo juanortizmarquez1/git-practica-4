09ffe9fa71403dfc549347bd54b416585cd1df6d
Juan Ortiz Márquez <juanortizmarquez005@gmail.com>
2021-08-30 14:32:17 +0200

    nueva2

--- clientes.sql
+++ clientes.sql
@@ -2,8 +2,8 @@ create table clientes
 (
     id int primary key serializable,
     nombre varchar(50),
-    apellidos varchar(60),
+    apellidos varchar(40),
     edad int,
-    direccion varchar(60)
+    direccion varchar(70)
 );
 select * from clientes
